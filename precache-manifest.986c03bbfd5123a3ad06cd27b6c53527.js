2de7c2a336f219fc9c1ac2376bcbe99efd71ced8
Dmitriy65 <mr.dima19980@gmail.com>
2020-02-16 22:46:22 +0300

    Updates

---
+++ precache-manifest.986c03bbfd5123a3ad06cd27b6c53527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4342a495cb49e369e78b77998fa3f8",
+    "url": "/ReactGallery/index.html"
+  },
+  {
+    "revision": "01e755cb77c9948cf9ef",
+    "url": "/ReactGallery/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "319919d977ab7110b3f0",
+    "url": "/ReactGallery/static/js/2.798903b2.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/ReactGallery/static/js/2.798903b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e755cb77c9948cf9ef",
+    "url": "/ReactGallery/static/js/main.be861fb9.chunk.js"
+  },
+  {
+    "revision": "8cd56fe26c647f244bb4",
+    "url": "/ReactGallery/static/js/runtime-main.fba99147.js"
+  }
+]);
